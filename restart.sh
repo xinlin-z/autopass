b65d18525b348c40ee005db0384a05bb3b73a810
xinllin <xinlin.zhang@hotmail.com>
2022-08-22 17:09:43 +0800

    add trap and kill9

--- restart.sh
+++ restart.sh
@@ -5,8 +5,17 @@ cd $dn
 passwd=$1
 shift
 
+trap 'kill9' SIGINT SIGTERM SIGKILL SIGHUP EXIT
+function kill9() {
+    kill -9 $child_pid
+    exit $?
+}
+
 while true; do
-    python3 -u autopass.py $passwd $@
+    echo ---- $(date) ----
+    python3 -u autopass.py $passwd $@ &
+    child_pid=$!
+    wait $child_pid
     sleep 8
 done
 
